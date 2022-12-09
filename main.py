2a0bc52064709573235e588386c4495f1a3c5ba9
AlexanderSychev2005 <alexandersychev2005@gmail.com>
2022-12-09 17:16:32 +0200

    some code

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
-with open("file.tsv", "r") as file:
-    lines = file.readlines()
+import sys
 
-print(lines[:5])
+
+def total():
+    year = sys.argv[3]
+    for line in lines:
+        line = line.split("\t")
+        if year == line[9]:
+            print(line)
+
+
+def medals():
+    country = sys.argv[3]
+
+
+data_file = sys.argv[1]
+with open("data_file.tsv", 'r') as file:
+    lines = file.readlines()
+    next_line = file.readlines()
+
+
+mode = sys.argv[2]  # medals or total
+
+if mode == "-total":
+    total()
+else:
+    medals()
