4bd77a80588af6f5d638c3c1ffe3cf2ab8e748f4
Maryna <marynachernetska@gmail.com>
2022-12-15 16:24:15 +0200

    Improve8

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sys
 import medals
 import total
+import overall
+
 data_file = sys.argv[1]
 with open("data_file.tsv", 'r') as file:
     lines = file.readlines()
@@ -13,3 +15,6 @@ if mode == "-total":
     total.total()
 elif mode == "-medals":
     medals.medals()
+elif mode == "-overall":
+    overall.overall()
+
